072290312ea3505682f2ae5169c96fb2bbbc9f6d
redev.rx <redev.rx@gmail.com>
2026-09-16 21:27:37 +0700

    fix(webtoon-age-limit): hide #_dimForPopup backdrop and unlock body/html scroll (1.2.1)

--- effects/webtoon-age-limit/main.js
+++ effects/webtoon-age-limit/main.js
@@ -1,18 +1,41 @@
 mangax.effect(function (ctx) {
   ctx.addStyle(
-    '.ly_wrap:has(.ly_adult), .ly_wrap:has(.age_limit), .ly_adult, .age_limit, [data-mx-webtoon-age-hidden] {\n' +
+    'body:has(.ly_adult) #_dimForPopup, body:has(.ly_adult) .ly_dim,\n' +
+    'body:has(.age_limit) #_dimForPopup, body:has(.age_limit) .ly_dim,\n' +
+    '.ly_wrap:has(.ly_adult), .ly_wrap:has(.age_limit),\n' +
+    '.ly_adult, .age_limit, [data-mx-webtoon-age-hidden] {\n' +
     '  display: none !important;\n' +
     '}\n' +
-    'body[data-mx-webtoon-age-unlocked] {\n' +
+    'body:has(.ly_adult), html:has(.ly_adult),\n' +
+    'body:has(.age_limit), html:has(.age_limit),\n' +
+    'body[data-mx-webtoon-age-unlocked], html[data-mx-webtoon-age-unlocked] {\n' +
     '  overflow: visible !important;\n' +
     '}'
   );
+
+  function unlock() {
+    document.body.setAttribute('data-mx-webtoon-age-unlocked', '');
+    if (document.documentElement) {
+      document.documentElement.setAttribute('data-mx-webtoon-age-unlocked', '');
+    }
+    var dim = document.getElementById('_dimForPopup') || document.querySelector('.ly_dim');
+    if (dim) {
+      dim.setAttribute('data-mx-webtoon-age-hidden', '');
+    }
+  }
 
   ctx.observe('.ly_adult, .age_limit', function (el) {
     var wrap = el.closest ? el.closest('.ly_wrap') : null;
     var target = wrap || el;
     target.setAttribute('data-mx-webtoon-age-hidden', '');
-    document.body.setAttribute('data-mx-webtoon-age-unlocked', '');
+    unlock();
+  });
+
+  ctx.observe('#_dimForPopup, .ly_dim', function (dim) {
+    if (document.querySelector('.ly_adult, .age_limit')) {
+      dim.setAttribute('data-mx-webtoon-age-hidden', '');
+      unlock();
+    }
   });
 
   return function () {
@@ -20,5 +43,8 @@ mangax.effect(function (ctx) {
       el.removeAttribute('data-mx-webtoon-age-hidden');
     });
     document.body.removeAttribute('data-mx-webtoon-age-unlocked');
+    if (document.documentElement) {
+      document.documentElement.removeAttribute('data-mx-webtoon-age-unlocked');
+    }
   };
 });
